70e6ed1acc1f6fd179b7e390f8503c22a844965a
Ulia <cs-gorodok@yandex.ru>
2023-10-08 17:09:30 +0300

    change src

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-        public int calculate (int bodyweight, double bodyheight) {
-            double bmi = bodyweight / (bodyheight * bodyheight);
-            return (int) bmi;
-        }
+    public int calculate(int bodyweight, double bodyheight) {
+        double bmi = bodyweight / (bodyheight * bodyheight);
+        return (int) bmi;
     }
+}
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bodyweight  = 98;
-        double bodyheight  = 1.87;
+        int bodyweight = 98;
+        double bodyheight = 1.87;
         int bmi = service.calculate(bodyweight, bodyheight);
         System.out.println("Индекс массы тела " + bmi + " кг/м2");
     }
